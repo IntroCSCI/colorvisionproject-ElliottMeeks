68ebe605287914142ded02083d9214d2711831f7
Elliott Meeks <emeeks@mail.csuchico.edu>
2020-11-17 05:06:10 +0000

    V2

---
+++ Other/README.md
@@ -0,0 +1,71 @@
+# v0.2 Updates
+IMAGE CHANGER
+
+## Description
+
+My challenge I came up with for myself was to create a program that could change 
+the color of a image to help color blind people while keeping the integrety of the image.
+By integrety I mean the basic color of the image. The program seems to work well for 
+helping colorblind people. I used a colorblind simulator to test out my altered images and 
+the numbers could be seen easily.
+
+## Developer
+
+Elliott Meeks
+
+## C++ Guide
+
+### Algerithem Explination
+
+What I found out is that what defines a color is the ratio of red to green to blue of each pixle. My algerithem keeps 
+the red, green, blue ratio but maximizes it. so the largest of the three will go to the value 255 while 
+the others will keep the original ratio. this will keep the color of the original image. 
+
+What I suspect this does is it maximizes the brightness of the image. Maybe decreases the contrast. I am 
+still not supper sure what it exaclt does but it works and seems to fix the colorblind problem with the testing that I 
+have done. 
+
+### Variables and Data Types
+
+Pixel and Bitmap Classes are used to get and alter the pixles of a bmp image.
+width and height variebles are important to make sure the program will work with any sized image.
+ 
+
+### Input and Output
+
+Gets user input to select which file they would like to change. Out puts the altered image and the program also 
+prompts the user in the console.
+
+### Decisions
+
+I used if statements to find the greatest of three numbers as part of my algerithem.
+in the bitmap.h library I made it if the r,g,b values exceded 255 or was less than 0 than it would set the value to 255 and 0 respectively.
+
+### Iteration
+
+nested for loop to go through each pixle
+
+### File Input and Output
+
+inputs a bmp image and alters the colors slightly in order to help colorblind people.
+
+### Arrays/Vectors
+vector < vector <Pixel> > bmp 
+2D Vector that gets the Pixel from every coordinant of the image.
+
+### Functions
+
+void changeColor(Pixel &, int,int,int)
+function to change the rgb values of the image. used in pixles()
+
+void pixles(Pixel &, vector <vector <Pixel>> &,int, int)
+locates each pixle in the image and gets it orginal rgb values and changes them.
+
+int findGreatest(int , int, int)
+finds the greates of red, green and blue.
+
+int algerithem(int , double)
+maximises the ration of red green and blue. SEE Algerithem Explination.
+
+string menue()
+generates the starting image and gets the user input to choose an image to change
--- main.cpp
+++ main.cpp
@@ -4,21 +4,98 @@
 
 using namespace std;
 
-Bitmap image;
-vector <vector <Pixel> > bmp;
-Pixel rgb;
+// pototypes
+void changeColor(Pixel &, int,int,int); // changes the color rgb values in each pixle
+void pixles(Pixel &, vector <vector <Pixel>> &,int, int); // locates all the pixles
+int findGreatest(int , int, int); // find the largests of red,green,blue
+int algerithem(int , double);  // makes colors more viberant
+string menue(); // starting menue
 
-void changeColor(int r, int g, int b)
+int main()
 {
+  Bitmap image;
+  Pixel rgb;
+  vector <vector <Pixel> > bmp;
 
-  int greyScale = (r+g+b)/2;
- 
-  rgb.red = greyScale;
-  rgb.green = greyScale;
-  rgb.blue = greyScale;
+  string name = menue(); // generates menue and gets user input
+  image.open(name); // Opens Image
+
+  int height = image.height();
+  int width = image.width();
+
+  //verify that the file opened was a valid image
+  bool validBmp = image.isImage();
+
+  if( validBmp == true )
+  {
+    bmp = image.toPixelMatrix(); // Images Pixles to a vector of vectors 
+
+    pixles(rgb, bmp ,width, height); // Alters the color of each pixle to be seen more easily
+
+    // saves image
+    image.fromPixelMatrix(bmp);
+    image.save("NEW.bmp");
+
+    cout << "Find the NEW.bmp on the LEFT side of the SCREEN! "<< endl;
+  }
+  
+  return 0;
 }
 
-void pixles(int width, int height)
+string menue(){
+
+  string name = "IMGi.bmp";
+  string prompt = "i) IMGi.bmp\n";
+  cout << "Which Image Would You Like To Open \n";
+
+  // this works lol
+  for (char i = '1'; i<='5'; i++){
+    prompt[0] = i;
+    prompt[6] = i;
+    cout << prompt;
+  }
+
+  char ans;
+  cin >> ans;
+
+  while (!(ans <='5' && ans >= '1')){
+    cout<< "Please Pick a Number Between 1-5 \n";
+    cin >> ans;
+  }
+
+  name[3] = ans;
+  return name;
+}
+
+int findGreatest(int r, int g, int b) 
+{
+    if(r>=b && r>=g){
+    return r;
+  } else if (b>=r && b>=g){
+    return b;
+  } else if (g>=b && g>=r){
+    return g;
+  }
+  return 0;
+}
+
+int algerithem(int color, double largest)
+{
+  int newColor = 255.0*(color/largest); 
+  return newColor;
+}
+
+
+void changeColor(Pixel & rgb, int r, int g, int b) 
+{
+  int largest = findGreatest(r,g,b);
+  
+  rgb.red = algerithem(r,largest);
+  rgb.green = algerithem(g,largest);
+  rgb.blue = algerithem(b,largest);
+}
+
+void pixles(Pixel & rgb, vector <vector <Pixel>> & bmp, int width, int height) 
 {
 
   for(int i = 0; i<height; i++)
@@ -31,33 +108,9 @@ void pixles(int width, int height)
       int g = rgb.green;
       int b = rgb.blue;
 
-      changeColor(r,g,b);
+      changeColor(rgb, r,g,b);
 
       bmp[i][j] = rgb;
     }  
   }  
-}
-
-int main()
-{
-  //read a file example.bmp and convert it to a pixel matrix
-  image.open("test4.bmp");
-
-  int height = image.height();
-  int width = image.width();
-
-  //verify that the file opened was a valid image
-  bool validBmp = image.isImage();
-
-  if( validBmp == true )
-  {
-    bmp = image.toPixelMatrix();
-
-    pixles(width, height);
-
-    image.fromPixelMatrix(bmp);
-    image.save("BlackWhite.bmp");
-  }
-  cout << "done!"<< endl;
-  return 0;
 }
